657da51a3e29279536bac92ce9095cf3b93112ab
Mubashar <mubasharnisar32@gmail.com>
2024-01-10 03:13:31 +0500

    scope to fetch all completed buy orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,8 @@
 class Order < ApplicationRecord
   enum order_type: { buy: 0, sell: 1 }
   enum status: { pending: 0, completed: 1, canceled: 2 }
+
+
+  scope :completed_buy_orders, -> { where(order_type: :buy, status: :completed) }
 end
 
